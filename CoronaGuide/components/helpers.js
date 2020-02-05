de21aabb012b6cd6a54664b24de0e69a4ba62976
levimake <levimake10@gmail.com>
2020-02-06 01:29:08 +0530

    final aayikondirikunu

--- CoronaGuide/components/helpers.js
+++ CoronaGuide/components/helpers.js
@@ -148,7 +148,8 @@ const helpers = {
       bannerSize="fullBanner"
       adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
       servePersonalizedAds // true or false
-      onDidFailToReceiveAdWithError={this.bannerError} />
+      onDidFailToReceiveAdWithError={this.bannerError}
+      style={{ backgroundColor: '#fff' }}/>
     );
   },
 
